1b873c64e461a4defeb6b57fb238dbd1bfcdf8e1
Lilli <lillichuawiwat@gmail.com>
2024-03-04 22:09:23 +1100

    Allowed for attack speed to be custom to each combat style

--- Mini RPG/Assets/Inputs/PlayerControls.cs
+++ Mini RPG/Assets/Inputs/PlayerControls.cs
@@ -38,11 +38,11 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": ""Attack"",
-                    ""type"": ""PassThrough"",
+                    ""type"": ""Button"",
                     ""id"": ""54f1140c-75fd-4c3b-8e12-500ff00f8573"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
-                    ""interactions"": ""Hold,Tap"",
+                    ""interactions"": ""Press(behavior=2)"",
                     ""initialStateCheck"": false
                 },
                 {
